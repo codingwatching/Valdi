e72878a1258ffd62503931aad4eeb204f15bb9f8
Jessica Yen <jyen@snapchat.com>
2026-01-30 20:27:53 +0000

    Internal Change
    GitOrigin-RevId: c3b26dbd7f4c5d01e27e03379a2296d93f47b619

--- tools/ci/build_core_targets.sh
+++ tools/ci/build_core_targets.sh
@@ -9,9 +9,6 @@ set -x
 # Intended to be run from open_source/
 cd "$(dirname "${BASH_SOURCE[0]}")/../.."
 
-# Pre-fetch primary dependencies, limiting threads to reduce memory usage
-bzl fetch //valdi:valdi //valdi_core:valdi_core --loading_phase_threads=4
-
 # High level core targets
 bzl build //valdi:valdi 
 bzl build //valdi_core:valdi_core
@@ -22,7 +19,10 @@ bzl build //libs/dummy:dummy_android
 
 if [[ $(uname) != Linux ]] ; then
     # Hello world apps
+    # Pre-fetch primary dependencies, limiting threads to reduce memory usage
+    bzl fetch //apps/helloworld:hello_world_ios --loading_phase_threads=4
     bzl build //apps/helloworld:hello_world_ios
+    bzl fetch //apps/helloworld:hello_world_macos --loading_phase_threads=4
     bzl build //apps/helloworld:hello_world_macos
 
     # Android depencencies have issues in ci, needs to be fixed
