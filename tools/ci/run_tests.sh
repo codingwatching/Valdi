5f35add3bc61d53891be3e7aaef4c335de85caad
Brian Holmes <bholmes@snap.com>
2026-07-06 14:19:39 -0700

    Internal Change
    GitOrigin-RevId: 938ab1dd5e28a3ebc124a8897ce7e46734bd90e0

--- tools/ci/run_tests.sh
+++ tools/ci/run_tests.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
-set -x
+set -eux
+
+(
 
 # Intended to be run from open_source/
+cd "$(dirname "$0")/../.."
 
 bzl test //valdi:test_snap_drawing //valdi:test_hermes --test_output=errors
 bzl test //valdi:test_layout --test_output=all --test_arg=--gtest_print_time=1
@@ -13,3 +15,5 @@ if [[ $(uname) != Linux ]] ; then
     bzl test //valdi:valdi_ios_swift_test
     bzl test //valdi:valdi_macos_objc_test
 fi
+
+)
